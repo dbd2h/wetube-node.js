3df92e676808cf51c76fd42a02cea623089a0b12
dbd2h <kangminchang1@gmail.com>
2022-02-21 02:03:31 +0900

    a

---
+++ src/controllers/videoController.js
@@ -0,0 +1,6 @@
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const search = (req, res) => res.send("Search");
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.send("Edit");
+export const deleteVideo = (req, res) => res.send("Delete Video");
+export const upload = (req, res) => res.send("Upload");
--- src/server.js
+++ src/server.js
@@ -8,9 +8,11 @@ const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
+console.log(process.cwd() + "/src/views");
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
 app.use(logger);
-
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
