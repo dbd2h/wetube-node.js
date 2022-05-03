60fda5b7172bcb3b4299d84495dfe2d5a78e7f54
강민창 <kangminchang@gangminchang-ui-MacBookAir.local>
2022-05-03 16:09:14 +0900

    a

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,8 @@
-export const join = (req, res) => res.send("Join");
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = (req, res) => {
+  console.log(req.body);
+  return res.render("join");
+};
 export const login = (req, res) => res.send("Login");
 export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 export const remove = (req, res) => res.send("Remove User");
