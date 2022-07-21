8b50dba517f32a812aeac5c5517df0b6274c0cd7
강민창 <kangminchang@gangminchang-ui-MacBookAir.local>
2022-07-22 05:12:19 +0900

    wetube 9.0~10.3

---
+++ src/client/js/main.js
@@ -0,0 +1,6 @@
+import "../scss/style.scss";
+
+for (let i = 0; i < 4; i++) {
+  console.log("HELLO WORLD");
+}
+console.log("GOODBYE WORLD");
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,7 +2,9 @@ import User from "../models/User";
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({})
+    .sort({ createdAt: "desc" })
+    .populate("owner");
   return res.render("home", { pageTitle: "Home", videos });
 };
 
@@ -13,7 +15,7 @@ export const search = async (req, res) => {
       title: {
         $regex: new RegExp(search, "i"),
       },
-    });
+    }).populate("owner");
     return res.render("searchResult", { pageTitle: "Search Result", videos });
   }
   return res.render("search", { pageTitle: "Search" });
